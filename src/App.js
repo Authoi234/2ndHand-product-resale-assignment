fd972eb4c98ff0ebfe2f82ebfe77d5295ce90770
Authoi234 <jawadkhan.authoi@gmail.com>
2024-03-02 12:51:35 +0600

    error is gone of auth context provider

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import routes from './Router/Routes';
 function App() {
   return (
     <div className="App">
-        <RouterProvider router={routes}></RouterProvider>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthContext } from './Contexts/AuthContextProvider';
+import AuthContextProvider from './Contexts/AuthContextProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-
-const queryClient = new QueryClient();
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthContext>
+      <AuthContextProvider>
         <App />
-      </AuthContext>
+      </AuthContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
