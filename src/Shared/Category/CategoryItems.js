47fe302cd9faefc0e60c81b31d11c5dc0c92c76d
Authoi234 <jawadkhan.authoi@gmail.com>
2024-03-03 23:56:31 +0600

    example words are removed

--- src/Shared/Category/CategoryItems.js
+++ src/Shared/Category/CategoryItems.js
@@ -28,8 +28,6 @@ const CategoryItems = () => {
             </div>
             <dialog id="order-booking-modal" className="modal">
                 <div className="modal-box">
-                    <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">Press ESC key or click the button below to close</p>
                     <form onSubmit={handleOrderSubmit}>
                         <label className="form-control w-full">
                             <div className="label"><span className="label-text font-semibold">Buyer Name</span></div>
