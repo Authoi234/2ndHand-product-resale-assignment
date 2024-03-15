7021d53d941426456aab586830e54fc1c8a560bd
Authoi234 <jawadkhan.authoi@gmail.com>
2024-03-15 19:48:33 +0600

    add a product form is styled git push

--- src/Pages/Seller/AddProduct/AddProduct.js
+++ src/Pages/Seller/AddProduct/AddProduct.js
@@ -5,23 +5,23 @@ const AddProduct = () => {
         <div>
             <h1 className='text-2xl'>Add A Product</h1>
             <form>
-                <label className="form-control w-full">
-                    <div className="label"><span className="label-text font-semibold">Product Name</span></div>
+                <label className="form-control md:max-w-md w-full my-0 mx-auto">
+                    <div className="label"><span className="label-text font-semibold text-xl">Product Name</span></div>
                     <input className="input input-bordered w-full" name='name' type="text" required />
                 </label>
 
-                <label className="form-control w-full">
-                    <div className="label"><span className="label-text font-semibold">Resale Price</span></div>
+                <label className="form-control md:max-w-md w-full my-0 mx-auto">
+                    <div className="label"><span className="label-text font-semibold text-xl">Resale Price</span></div>
                     <input className="input input-bordered w-full" name='resalePrice' type="number" required />
                 </label>
 
-                <label className="form-control w-full">
-                    <div className="label"><span className="label-text font-semibold">Original Price</span></div>
+                <label className="form-control md:max-w-md w-full my-0 mx-auto">
+                    <div className="label"><span className="label-text font-semibold text-xl">Original Price</span></div>
                     <input className="input input-bordered w-full" name='originalPrice' type="number" required />
                 </label>
 
-                <label className="form-control w-full">
-                    <div className="label"><span className="label-text font-semibold">Condition Type</span></div>
+                <label className="form-control md:max-w-md w-full my-0 mx-auto">
+                    <div className="label"><span className="label-text font-semibold text-xl">Condition Type</span></div>
                     <select className='select select-bordered' name='condition'>
                         <option value="Excellent" selected>Excellent</option>
                         <option value="Good">Good</option>
@@ -29,37 +29,37 @@ const AddProduct = () => {
                     </select>
                 </label>
 
-                <label className="form-control w-full">
-                    <div className="label"><span className="label-text font-semibold">Mobile Number</span></div>
+                <label className="form-control md:max-w-md w-full my-0 mx-auto">
+                    <div className="label"><span className="label-text font-semibold text-xl">Mobile Number</span></div>
                     <input className="input input-bordered w-full" name='mobile' type="number" required />
                 </label>
 
-                <label className="form-control w-full">
-                    <div className="label"><span className="label-text font-semibold">Location</span></div>
+                <label className="form-control md:max-w-md w-full my-0 mx-auto">
+                    <div className="label"><span className="label-text font-semibold text-xl">Location</span></div>
                     <input className="input input-bordered w-full" name='location' type="text" required />
                 </label>
 
-                <label className="form-control w-full">
-                    <div className="label"><span className="label-text font-semibold">Product Category</span></div>
+                <label className="form-control md:max-w-md w-full my-0 mx-auto">
+                    <div className="label"><span className="label-text font-semibold text-xl">Product Category</span></div>
                     <input className="input input-bordered w-full" name='category' type="text" required />
                 </label>
 
-                <label className="form-control w-full">
-                    <div className="label"><span className="label-text font-semibold">Discription</span></div>
+                <label className="form-control md:max-w-md w-full my-0 mx-auto">
+                    <div className="label"><span className="label-text font-semibold text-xl">Discription</span></div>
                     <input className="input input-bordered w-full" name='discription' type="text" required />
                 </label>
 
-                <label className="form-control w-full">
-                    <div className="label"><span className="label-text font-semibold">Years of use</span></div>
+                <label className="form-control md:max-w-md w-full my-0 mx-auto">
+                    <div className="label"><span className="label-text font-semibold text-xl">Years of use</span></div>
                     <input className="input input-bordered w-full" name='yearsofuse' type="number" required />
                 </label>
 
-                <label className="form-control w-full">
-                    <div className="label"><span className="label-text font-semibold">Years of purchase</span></div>
+                <label className="form-control md:max-w-md w-full my-0 mx-auto">
+                    <div className="label"><span className="label-text font-semibold text-xl">Years of purchase</span></div>
                     <input className="input input-bordered w-full" name='yearsofbuy' type="number" required />
                 </label>
                 
-                <button type="submit" className='btn btn-primary my-2'>Submit</button>
+                <button type="submit" className='btn btn-primary w-full md:max-w-md my-5'>Submit</button>
             </form>
         </div>
     );
