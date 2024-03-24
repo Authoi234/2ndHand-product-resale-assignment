e00bc41b1950b3aef8638839f2350fd4a8611ba7
Authoi234 <jawadkhan.authoi@gmail.com>
2024-03-25 00:48:16 +0600

    add products route is complete

--- src/Pages/Seller/AddProduct/AddProduct.js
+++ src/Pages/Seller/AddProduct/AddProduct.js
@@ -1,32 +1,88 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import '../../../App.css'
+import { format } from 'date-fns';
+import { AuthContext } from './../../../Contexts/AuthContextProvider';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const AddProduct = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, reset } = useForm();
+    const navigate = useNavigate();
     const [date, setDate] = useState(new Date());
+    const {user} = useContext(AuthContext);
+
+    const imageHostKey = process.env.REACT_APP_ImgbbHostKey;
 
     const handleAddProduct = (data) => {
+        const dateOfPost = format(date, "PP")
 
         const image = data.image[0];
         const formData = new FormData();
         formData.append('image', image);
-        console.log(formData)
 
-        fetch(`https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_ImgbbHostKey}`, {
+        fetch(`https://api.imgbb.com/1/upload?key=${imageHostKey}`, {
             method: 'POST',
             body: formData
         }).then(res => res.json())
-        .then(data => {
-            if (data.success) {
+        .then(result => {
+            if (result.success || result.status === 200) {
+                const type = data.category.split(' ')[1];
+                const categoryId = parseInt(data.category.split(' ')[0]);
+                const img = result.data.url;
+                const name = data.name;
+                const location = data.location;
+                const resalePrice = parseInt(data.resalePrice);
+                const originalPrice = parseInt(data.originalPrice);
+                const yearsOfUse = parseInt(data.yearsofuse);
+                const discription = data.discription;
+                const timeWhenItPosted = dateOfPost;
+                const sellersName = user.displayName;
+                const isUserVerified = false;
+                const conditionType = data.condition;
+                const yearOfPurchase = parseInt(data.yearofbuy);
+                const mobile = parseInt(data.mobile);
                 
+                const product = {
+                    categoryId,
+                    img,
+                    name,
+                    location,
+                    resalePrice,
+                    originalPrice,
+                    yearsOfUse,
+                    timeWhenItPosted,
+                    sellersName,
+                    type,
+                    isUserVerified,
+                    conditionType,
+                    discription,
+                    yearOfPurchase,
+                    mobile
+                }
+
+                fetch(`http://localhost:5000/addProduct`, {
+                    method: 'POST',
+                    headers: {
+                        'content-type' : 'application/json'
+                    },
+                    body: JSON.stringify(product)
+                })
+                .then(res => {
+                    res.json();
+                })
+                .then(data => {
+                    console.log(data)
+                    toast.success('Product is added');
+                    reset();
+                })
             }
         })
     }
 
     return (
         <div className='transparent-form'>
-            <h1 className='text-3xl text-white font-semibold mb-3   '>Add A Product</h1>
+            <h1 className='text-3xl text-white font-semibold mb-3'>Add A Product</h1>
                 <form onSubmit={handleSubmit(handleAddProduct)} className='md:max-w-lg mx-auto my-0 p-5 '>
                     <label className="form-control md:max-w-md w-full my-0 mx-auto">
                         <div className="label"><span className="label-text font-semibold text-xl text-white">Product Name</span></div>
@@ -65,9 +121,9 @@ const AddProduct = () => {
                     <label className="form-control md:max-w-md w-full my-0 mx-auto">
                         <div className="label"><span className="label-text font-semibold text-xl text-white">Product Category</span></div>
                         <select className='select select-bordered  text-white' style={{backgroundColor: 'rgba(0,0,0, 0.5)'}} name='category' {...register("category")}>
-                            <option value="Supercar" selected>Supercar</option>
-                            <option value="SUV car">SUV car</option>
-                            <option value="Sedan car">Sedan car</option>
+                            <option value="1 Supercar" selected>Supercar</option>
+                            <option value="2 SUVcar">SUV car</option>
+                            <option value="3 Sedancar">Sedan car</option>
                         </select>
                     </label>
 
