57031142ac7a2d534485ec110d3c0cf86c3264ef
Authoi234 <jawadkhan.authoi@gmail.com>
2024-03-25 23:17:19 +0600

    product is delete button is ready and its working

--- src/Pages/Seller/MyProducts/MyProducts.js
+++ src/Pages/Seller/MyProducts/MyProducts.js
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { AuthContext } from '../../../Contexts/AuthContextProvider';
+import toast from 'react-hot-toast';
 
 const MyProducts = () => {
     const { user } = useContext(AuthContext);
+    const [modalData, setModalData] = useState(null);
 
     const { data: products = [], isPending, error } = useQuery({
         queryKey: ['products'],
@@ -13,6 +15,23 @@ const MyProducts = () => {
             return data;
         }
     })
+
+    const handleDelete = (product) => {
+        fetch(`http://localhost:5000/products/${product._id}`, {
+            method: 'Delete',
+            headers: {
+
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if (data.acknowledged) {
+                toast.success(`Successfully Deleted ${product.name}`);
+                document.getElementById('delete-confirm-modal').close();
+            }
+        })
+    }
 
     return (
         <div>
@@ -31,17 +50,31 @@ const MyProducts = () => {
                         </tr>
                     </thead>
                     <tbody>
-                       {products.map((product, i )=>  <tr className='hover'>
+                        {products.map((product, i) => <tr className='hover'>
                             <th>{i + 1}</th>
                             <td><img className='avatar w-20 rounded-full' src={product.img} alt="" /></td>
                             <td className='font-bold'>{product.name}</td>
                             <td>${product.resalePrice}</td>
                             <td>{product.status}</td>
-                            <td><button className='btn bg-red-500 border border-red-500 rounded-none text-white hover:bg-white hover:text-red-500'>  </button></td>
+                            <td><button onClick={() => {document.getElementById('delete-confirm-modal').showModal(); setModalData(product)}} className='btn bg-red-500 border-2 hover:border-red-500 rounded-none text-white hover:bg-white hover:text-red-500'> Delete </button></td>
+                            <td><button className='btn bg-cyan-400 border-2 hover:border-cyan-400 hover:scale-105 rounded-none text-white hover:bg-white hover:text-cyan-400'> Advertise </button></td>
                         </tr>)}
                     </tbody>
                 </table>
             </div>
+            <dialog id="delete-confirm-modal" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Are you sure ? you want to Delete</h3>
+                    <p className="py-4">It will be deleted permanently, and cant be undone.</p>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn btn-neutral" onClick={() => setModalData(null)}>Close</button>
+                        </form>
+                        <button className='btn btn-primary' onClick={() => handleDelete(modalData)}>Delete</button>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 };
