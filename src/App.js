721e0f785ff8251e8b94e2344e0ba842029a0fda
Authoi234 <jawadkhan.authoi@gmail.com>
2024-05-16 19:33:16 +0700

    preloader is removed because of hanging

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import routes from './Router/Routes';
 import { Toaster } from 'react-hot-toast';
-import preloaderImg from './assets/images/2eVCvQ-LogoMakr.png';
+// import preloaderImg from './assets/images/2eVCvQ-LogoMakr.png';
 
 function App() {
 
-  window.addEventListener('load', () => {
-    const loader = document.getElementById("preloader");
-    loader.style.display = "none"; 
-  });
+  // window.addEventListener('load', () => {
+  //   const loader = document.getElementById("preloader");
+  //   loader.style.display = "none"; 
+  // });
 
 
   return (
     <div className="App">
-      <div id="preloader" className='flex justify-center items-center'>
+      {/* <div id="preloader" className='flex justify-center items-center'>
         <div>
           <img className='animate-bounce' src={preloaderImg} alt="" />
           <div className="flex items-center justify-center">
@@ -23,7 +23,7 @@ function App() {
             <span className="loading loading-dots w-10 bg-accent"></span>
           </div>
         </div>
-      </div>
+      </div> */}
       <RouterProvider router={routes}></RouterProvider>
       <Toaster></Toaster>
     </div>
